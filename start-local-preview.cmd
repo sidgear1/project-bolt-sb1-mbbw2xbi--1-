e3cee10add9507c91f7b709655b201ad91362a40
David <sidgear1@gmail.com>
2026-08-18 01:40:13 +0100

    Move Vite app to repository root for Bolt

--- start-local-preview.cmd
+++ start-local-preview.cmd
@@ -1,5 +1,5 @@
 @echo off
-title Memorie Perdute - Local Preview
-cd /d "%~dp0project"
+title TaleTalk - Local Preview
+cd /d "%~dp0"
 "C:\Program Files\nodejs\node.exe" node_modules\vite\bin\vite.js --host 0.0.0.0 --port 5173 --strictPort
 pause
